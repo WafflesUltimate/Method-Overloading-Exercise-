e197ff4fe15a63e0e2d68f309d4af8c002e6ec67
David Armstrong <wafflesultimate1@gmail.com>
2023-10-06 11:40:02 -0700

    Fixed problems

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -5,14 +5,16 @@
 
         static int Add(int num1, int num2)
         {
-
-            return num1 + num2;
+            int numTotal = num1 + num2;
+            Console.WriteLine(numTotal);
+            return numTotal;
         }
 
         static decimal Add(decimal num1, decimal num2)
         {
-
-            return num1 + num2;
+            decimal numTotal = num1 + num2;
+            Console.WriteLine(numTotal);
+            return numTotal;
         }
 
         static string Add(int num1, int num2, bool isTrue)
@@ -24,11 +26,15 @@
             Console.WriteLine(t);
 
             return (isTrue && numTotal > 1 && numTotal != '-') ? numTotal + " dollars" : 
-            (isTrue && numTotal > 0 && numTotal != '-') ? numTotal + " dollar" : $"You cannot have {numTotal} dollars.";
+            (isTrue && numTotal < 0 && numTotal != '-') ? numTotal + " dollar" : $"You cannot have {numTotal} dollars.";
         }
 
         static void Main(string[] args)
         {
+            Add(4, 5);
+
+            Add(13.5m, 3.3m);
+
             Add(-5, 2, true);
         }
     }
